b0b7a089888ab662b66dc59b227f643e89321861
ccy1563 <ccy1563@gmail.com>
2021-09-16 05:06:00 -0400

    Bug Fix Attempt

--- frontend/components/cart_items/cart_item_index_item.jsx
+++ frontend/components/cart_items/cart_item_index_item.jsx
@@ -46,10 +46,6 @@ class CartItemIndexItem extends React.Component {
     }
 
     render() {
-        // debugger 
-
-        // debugger
-
         // debugger
         const {
             product_id,
