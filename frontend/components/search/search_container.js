f77a6fdc0934994ec6643af87d2300fd15fbf38d
ccy1563 <ccy1563@gmail.com>
2021-10-23 23:50:16 -0400

    search container

--- frontend/components/search/search_container.js
+++ frontend/components/search/search_container.js
@@ -2,4 +2,13 @@ import { connect } from 'react-redux';
 import React from 'react';
 import Search from './search';
 
+const mSTP = state => {
+
+}
+
+const mDTP = dispatch => ({
+    
+})
+
 export default connect(null, null)(Search);
+
