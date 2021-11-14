ffdd15788a24a506bb186394fbdd63d0550fed05
ccy1563 <ccy1563@gmail.com>
2021-11-14 04:37:55 -0500

    product show page alignment

--- frontend/components/reviews/review_form.jsx
+++ frontend/components/reviews/review_form.jsx
@@ -46,20 +46,21 @@ class ReviewForm extends React.Component {
         // debugger;
         return (
             <div className="review-form">
-                <form onSubmit={(e) => this.handleSubmit(e)}>
+                <div>
+                    <form onSubmit={(e) => this.handleSubmit(e)}>
 
-                    <Box component="fieldset" mb={3} borderColor="transparent">
-                        <Rating
-                            style={{ color: "black" }}
-                            name="Rating Label"
-                            value={this.state.rating}
-                            precision={0.5}
-                            onChange={this.update("rating")}
-                        />
-                    </Box>
+                        <Box component="fieldset" mb={3} borderColor="transparent">
+                            <Rating
+                                style={{ color: "black" }}
+                                name="Rating Label"
+                                value={this.state.rating}
+                                precision={0.5}
+                                onChange={this.update("rating")}
+                            />
+                        </Box>
 
 
-                    {/* <label>Rating</label>
+                        {/* <label>Rating</label>
                     <input
                         type="number"
                         max="5"
@@ -67,17 +68,20 @@ class ReviewForm extends React.Component {
                         value={this.state.rating}
                         onChange={this.update("rating")}
                     /> */}
-                    {/* <label>Review</label> */}
-                    <textarea
-                        className="create-review-textarea"
-                        id="review-submit-button"
-                        cols="65"
-                        rows="4"
-                        value={this.state.body}
-                        onChange={this.update("body")}
-                    />
-                    <input type="submit" className="review-submit-bttn" />
-                </form>
+                        {/* <label>Review</label> */}
+                        <textarea
+                            className="create-review-textarea"
+                            id="review-submit-button"
+                            cols="65"
+                            rows="4"
+                            value={this.state.body}
+                            onChange={this.update("body")}
+                        />
+                        <div className='review-submit-bttn-box'>
+                            <input type="submit" className="review-submit-bttn" />
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
